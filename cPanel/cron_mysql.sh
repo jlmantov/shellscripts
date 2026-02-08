7c8173793696460277b14eac639e1465d240429e
jlm <janick.mantov@gmail.com>
2026-02-08 12:14:34 +0100

    Adds member_email cleanup to cron_mysql.sh

--- cPanel/cron_mysql.sh
+++ cPanel/cron_mysql.sh
@@ -14,15 +14,42 @@ echo "DBUSR: $DBUSR" >$LOGFILE 2>&1
 echo "DBNAME: $DEVAPIDB"  >>$LOGFILE 2>&1
 echo "------------"  >>$LOGFILE 2>&1
 echo >>$LOGFILE 2>&1
-echo "SQL: 'SELECT count(*) FROM session WHERE expiresAt > NOW();'"  >>$LOGFILE 2>&1
-echo "------------"  >>$LOGFILE 2>&1
-/usr/bin/mysql -u $DBUSR --password="$DBPWD" $DEVAPIDB --skip-column-names -e 'SELECT * FROM session WHERE expiresAt > NOW();' >>$LOGFILE 2>&1
+
+# Cleanup expired sessions
+SQL="SELECT count(*) FROM session WHERE expiresAt < NOW() - INTERVAL 1 MINUTE;"
+echo "SQL: '$SQL'"  >>$LOGFILE 2>&1
+/usr/bin/mysql -u $DBUSR --password="$DBPWD" $DEVAPIDB --skip-column-names -e "$SQL" >>$LOGFILE 2>&1
 echo >>$LOGFILE 2>&1
 
-# Cleanup expires sessions
-echo "SQL: 'DELETE FROM session WHERE expiresAt < NOW() - INTERVAL 1 MINUTE;'"  >>$LOGFILE 2>&1
+SQL="DELETE FROM session WHERE expiresAt < NOW() - INTERVAL 1 MINUTE;"  >>$LOGFILE 2>&1
+echo "SQL: '$SQL'"  >>$LOGFILE 2>&1
+/usr/bin/mysql -u $DBUSR --password="$DBPWD" $DEVAPIDB -e "$SQL" >>$LOGFILE 2>&1
 echo "------------"  >>$LOGFILE 2>&1
-/usr/bin/mysql -u $DBUSR --password="$DBPWD" $DEVAPIDB -e 'DELETE FROM session WHERE expiresAt < NOW() - INTERVAL 1 MINUTE;' >>$LOGFILE 2>&1
+echo >>$LOGFILE 2>&1
+
+# Cleanup 'updatepwd' confirmation email-links
+echo >>$LOGFILE 2>&1
+SQL="SELECT count(*) FROM member_email WHERE createdAt < NOW() - INTERVAL 10 MINUTE;"
+echo "SQL: '$SQL'"  >>$LOGFILE 2>&1
+/usr/bin/mysql -u $DBUSR --password="$DBPWD" $DEVAPIDB --skip-column-names -e "$SQL" >>$LOGFILE 2>&1
+echo >>$LOGFILE 2>&1
+
+SQL="DELETE FROM member_email WHERE createdAt < NOW() - INTERVAL 10 MINUTE;"
+echo "SQL: $SQL"  >>$LOGFILE 2>&1
+/usr/bin/mysql -u $DBUSR --password="$DBPWD" $DEVAPIDB -e "$SQL" >>$LOGFILE 2>&1
+echo "------------"  >>$LOGFILE 2>&1
+echo >>$LOGFILE 2>&1
+
+# Cleanup any confirmation email older than 7 days
+SQL="SELECT count(*) FROM member_email WHERE createdAt < NOW() - INTERVAL 7 DAY;"
+echo "SQL: $SQL"  >>$LOGFILE 2>&1
+/usr/bin/mysql -u $DBUSR --password="$DBPWD" $DEVAPIDB --skip-column-names -e "$SQL" >>$LOGFILE 2>&1
+echo >>$LOGFILE 2>&1
+
+SQL="DELETE FROM member_email WHERE createdAt < NOW() - INTERVAL 7 DAY;"
+echo "SQL: $SQL"  >>$LOGFILE 2>&1
+echo "------------"  >>$LOGFILE 2>&1
+/usr/bin/mysql -u $DBUSR --password="$DBPWD" $DEVAPIDB -e "$SQL" >>$LOGFILE 2>&1
 echo >>$LOGFILE 2>&1
 
 # Optional: Log the execution
